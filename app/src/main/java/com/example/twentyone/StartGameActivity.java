655e8adae2c03f9deba47e127add8f44534a6f40
Sliff-Zoschitan <99servey99@gmail.com>
2019-10-19 20:37:48 +0300

    Переделал начало игры

--- app/src/main/java/com/example/twentyone/StartGameActivity.java
+++ app/src/main/java/com/example/twentyone/StartGameActivity.java
@@ -1,57 +1,87 @@
 package com.example.twentyone;
 
-
-import android.content.Intent;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.SeekBar;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class StartGameActivity extends AppCompatActivity {
-
-    TextView textViewWins;
-    TextView textViewCoins;
-    TextView textView4;
-    TextView textView5;
-    TextView textView6;
-    TextView textView9;
-    TextView textView10;
-    TextView textView11;
-    TextView textView12;
+public class StartGameActivity extends AppCompatActivity implements SeekBar.OnSeekBarChangeListener{
+    TextView textViewType1;
+    TextView textViewType2;
+    TextView textViewType3;
+    TextView textViewPlayers2;
+    TextView textViewPlayers3;
+    TextView textViewPlayers4;
+    TextView textViewPlayers5;
+    TextView bet;
+    TextView players;
+    SeekBar sb;
+    SeekBar sb2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start_game);
-        textView4 = (TextView) findViewById(R.id.textView4);
-        textView5 = (TextView) findViewById(R.id.textView5);
-        textView6 = (TextView) findViewById(R.id.textView6);
-        textView9 = (TextView) findViewById(R.id.textView9);
-        textView10 = (TextView) findViewById(R.id.textView10);
-        textView11 = (TextView) findViewById(R.id.textView11);
-        textView12 = (TextView) findViewById(R.id.textView12);
-
-        textViewWins = (TextView) findViewById(R.id.textViewWins);
-        textViewCoins = (TextView) findViewById(R.id.textViewCoins);
-        Intent intent = getIntent();
-        String wins = intent.getStringExtra("wins");
-        textViewWins.setText(wins);
-        String coins = intent.getStringExtra("coins");
-        textViewCoins.setText(coins);
+        textViewType1 = (TextView) findViewById(R.id.textViewType1);
+        textViewType2 = (TextView) findViewById(R.id.textViewType2);
+        textViewType3 = (TextView) findViewById(R.id.textViewType3);
+        bet = (TextView) findViewById(R.id.bet);
+        sb = (SeekBar) findViewById(R.id.sb);
+        players = (TextView) findViewById(R.id.players);
+        sb2 = (SeekBar) findViewById(R.id.sb2);
+        sb.setOnSeekBarChangeListener(this);
+        sb2.setOnSeekBarChangeListener(this);
     }
     public void onClickType(View v) {
-        textView4.setBackgroundResource(R.drawable.type);
-        textView5.setBackgroundResource(R.drawable.type);
-        textView6.setBackgroundResource(R.drawable.type);
+        textViewType1.setBackgroundResource(R.drawable.type);
+        textViewType2.setBackgroundResource(R.drawable.type);
+        textViewType3.setBackgroundResource(R.drawable.type);
         v.setBackgroundResource(R.drawable.type2);
     }
-    public void onClickPlayers(View v) {
-        textView9.setBackgroundResource(R.drawable.players);
-        textView10.setBackgroundResource(R.drawable.players);
-        textView11.setBackgroundResource(R.drawable.players);
-        textView12.setBackgroundResource(R.drawable.players);
-        v.setBackgroundResource(R.drawable.players2);
+
+    @Override
+    public void onProgressChanged(SeekBar sb, int i, boolean b) {
+        switch (sb.getId())
+        {
+            case R.id.sb:
+                bet.setText(String.valueOf(sb.getProgress())+"/3000");
+                break;
+            case R.id.sb2:
+                players.setText(String.valueOf(sb.getProgress())+"/5");
+                break;
+        }
+
+    }
+
+    @Override
+    public void onStartTrackingTouch(SeekBar sb) {
+
+    }
+
+    @Override
+    public void onStopTrackingTouch(SeekBar sb) {
+
+    }
+
+    public void onClick(View v) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(StartGameActivity.this);
+        builder.setTitle("info")
+                .setMessage("Тип1\n  Крч, братан, этот тип игры предполагает, что я в своём познании преисполнился." +
+                            "\nТип2\n  А этот тип заключается в том, что я как будто бы уже 100 триллионов миллиардов лет проживаю на триллионах и триллионах таких же планет." +
+                            "\nТип3\n  Ну а этот тип состоит в том, что мне уже этот мир абсолютно понятен, и я здесь ищу только одного: покоя, умиротворения и вот этой гармонии от слияния с бесконечно вечным.")
+                .setCancelable(false)
+                .setNegativeButton("ОК, спасибо за преисполнение",
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                dialog.cancel();
+                            }
+                        });
+        AlertDialog alert = builder.create();
+        alert.show();
     }
 }
 

d4c4b1778f3fe647ee7e5f6d8b633e26d0058583
Kurlin Alexandr <sasha-kurlin@mail.ru>
2019-10-25 23:49:50 +0300

    вечором

--- app/src/main/java/com/example/twentyone/TournamentActivity.java
+++ app/src/main/java/com/example/twentyone/TournamentActivity.java
@@ -1,9 +1,6 @@
 package com.example.twentyone;
 
-import android.annotation.TargetApi;
-import android.os.Build;
 import android.os.Bundle;
-import android.view.View;
 
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentActivity;
@@ -39,7 +36,7 @@ public class TournamentActivity extends FragmentActivity {
         }
         @Override
         public Fragment getItem(int position) {
-            return PageFragment.newInstance(position);
+            return TournamentFragment.newInstance(position);
         }
         @Override
         public int getCount() {
@@ -47,39 +44,5 @@ public class TournamentActivity extends FragmentActivity {
         }
     }
 
-    public class ZoomOutPageTransformer implements ViewPager.PageTransformer {
-        private  float MIN_SCALE = 0.85f;
-        private  float MIN_ALPHA = 0.5f;
 
-        @TargetApi(Build.VERSION_CODES.HONEYCOMB)
-        @Override
-        public void transformPage(View view, float position) {
-            int pageWidth = view.getWidth();
-            int pageHeight = view.getHeight();
-            if (position < -1) { // [-Infinity,-1)
-                // This page is way off-screen to the left.
-                view.setAlpha(0);
-            } else if (position <= 1) { // [-1,1]
-                // Modify the default slide transition to
-                // shrink the page as well
-                float scaleFactor = Math.max(MIN_SCALE, 1 - Math.abs(position));
-                float vertMargin = pageHeight * (1 - scaleFactor) / 2;
-                float horzMargin = pageWidth * (1 - scaleFactor) / 2;
-                if (position < 0) {
-                    view.setTranslationX(horzMargin - vertMargin / 2);
-                } else {
-                    view.setTranslationX(-horzMargin + vertMargin / 2);
-                }
-                // Scale the page down (between MIN_SCALE and 1)
-                view.setScaleX(scaleFactor);
-                view.setScaleY(scaleFactor);
-                // Fade the page relative to its size.
-                view.setAlpha(MIN_ALPHA + (scaleFactor - MIN_SCALE)
-                        / (1 - MIN_SCALE) * (1 - MIN_ALPHA));
-            } else { // (1,+Infinity]
-                // This page is way off-screen to the right.
-                view.setAlpha(0);
-            }
-        }
-    }
 }
